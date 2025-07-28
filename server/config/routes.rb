5bda519c9a78ce8162737c4eebbb6c23e39e5a69
Jai Rajput <jai@tecorb.co>
2025-07-28 18:39:39 +0530

    refactor(CE): Admin panel improved to track sync, new syncs features enabled

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
     get 'syncs', to: 'user_syncs#syncs_index'
     get 'syncs/:id', to: 'user_syncs#sync_show', as: 'sync'
     get 'syncs/:id/sync_run/:sync_run_id/records', to: 'user_syncs#sync_run_records', as: 'sync_run_records'
+    get 'syncs/:id/sync_run/:sync_run_id/logs', to: 'user_syncs#sync_run_logs', as: 'sync_run_logs'
 
     root 'users#index'
     get 'setting/index', to: 'settings#index'
